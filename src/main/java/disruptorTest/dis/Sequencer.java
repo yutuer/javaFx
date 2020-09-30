41169596d964f8c2dc366c98e31c579830542ad6
yutuer <yutuerzf@163.com>
2020-09-30 10:04:17 +0800

    1

--- src/main/java/disruptorTest/dis/Sequencer.java
+++ src/main/java/disruptorTest/dis/Sequencer.java
@@ -1,10 +1,11 @@
 package disruptorTest.dis;
 
-import disruptorTest.dis.Seqer;
+import disruptorTest.dis.consume.WaitStrategy;
 
 /**
  * @Description 序号产生器
- * 计数调度器超类, 环形数组原型
+ * <p>
+ * 计数调度器超类, 环形数组原型.  数组的约束用此类实现
  * @Author zhangfan
  * @Date 2020/9/29 7:40
  * @Version 1.0
@@ -14,9 +15,31 @@ public abstract class Sequencer implements Seqer
     // 一个数组大小, 用来获取下标. 所以是len-1
     protected int buffSize;
 
-    public Sequencer(int buffSize)
+    /**
+     * 生产者的序列，表示生产者的进度。
+     * PS: 代码里面的带cursor的都表示生产者们的Sequence。
+     * <p>
+     * 消费者与生产者之间的交互（可见性保证）是通过volatile变量的读写来保证的。
+     * 当前的序号
+     */
+    protected final Seq cursor = new Seq(Seq.INITIAL_VALUE);
+
+    /**
+     * 网关Sequences，序号生成器必须和这些Sequence满足约束:
+     * cursor-bufferSize <= Min(gatingSequence)
+     * 即：所有的gatingSequences让出下一个插槽后，生产者才能获取该插槽。
+     */
+    protected volatile Seq[] gatingSequences = new Seq[0];
+
+    /**
+     * 等待策略
+     */
+    protected WaitStrategy waitStrategy;
+
+    public Sequencer(int buffSize, WaitStrategy waitStrategy)
     {
         this.buffSize = buffSize;
+        this.waitStrategy = waitStrategy;
     }
 
 }
--- src/main/java/disruptorTest/dis/SingleThreadSequencer.java
+++ src/main/java/disruptorTest/dis/SingleThreadSequencer.java
@@ -1,5 +1,7 @@
 package disruptorTest.dis;
 
+import disruptorTest.dis.consume.WaitStrategy;
+
 /**
  * @Description 单线程计数器
  * @Author zhangfan
@@ -10,23 +12,26 @@ public class SingleThreadSequencer extends Sequencer
 {
 
     /**
-     * 预分配的生产者序列号
+     * 已预分配的缓存，因为是单线程的生产者，不存在竞争，因此采用普通的long变量
+     * 表示 {@link #cursor} +1 ~  nextValue 这段空间被预分配出去了，但是可能还未填充数据。
+     * 会在真正分配空间时更新
      */
-    protected long nextValue;
+    long nextValue;
 
     /**
-     * 当前的序号
+     * 网关序列的最小序号缓存。
+     * 因为是单线程的生产者，数据无竞争，因此使用普通的long变量即可。(为什么是单线程的?  应该是多线程的才对啊, 因为消费者是多个)
+     *
+     * Q: 该缓存值的作用？
+     * A: 除了直观上的减少对{@link #gatingSequences}的遍历产生的volatile读以外，还可以提高缓存命中率。
+     *
+     * PS: 使用一个变化频率较低的值代替一个变化频率较高的值，提高读效率。
      */
-    protected long cachedValue;
+    long cachedValue;
 
-    /**
-     * 消费者序号
-     */
-    protected long gatingValue;
-
-    public SingleThreadSequencer(int buffSize)
+    public SingleThreadSequencer(int buffSize, WaitStrategy waitStrategy)
     {
-        super(buffSize);
+        super(buffSize, waitStrategy);
     }
 
     @Override
@@ -35,9 +40,16 @@ public class SingleThreadSequencer extends Sequencer
         return next(1);
     }
 
-    private long next(int i)
+    private long next(int n)
     {
-        final long nextValue = this.nextValue;
+        if (n < 1)
+        {
+            throw new IllegalArgumentException("n must be > 0");
+        }
+
+        // 已分配的序号的缓存(已分配到这里)，初始-1
+        long nextValue = this.nextValue;
+
         long judgeValue = nextValue - buffSize;
 
 
